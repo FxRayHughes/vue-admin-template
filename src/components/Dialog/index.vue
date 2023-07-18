4379985e9c9277180a0f4aa4fbee02cbadc0a2f9
FxRayHughes <bushuaichen@yeah.net>
2023-07-18 13:15:51 +0800

    init

---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="open" class="f-dialog">
+    <dialog :id="id">
+      <slot />
+    </dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FDialog',
+  props: {
+    id: {
+      type: String,
+      default: 'dialog'
+    },
+    styleObject: {
+      type: Object,
+      default: () => {
+        return {
+          width: '100%',
+          height: '100%'
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      open: true
+    }
+  },
+  mounted() {
+    const styleString = Object.keys(this.styleObject)
+      .map(key => `${key}: ${this.styleObject[key]}`)
+      .join('; ')
+    const doc = document.getElementById(this.id)
+    doc.setAttribute('style', styleString)
+    doc.showModal()
+  },
+  methods: {
+    closeDialog() {
+      const doc = document.getElementById(this.id)
+      doc.close()
+      //
+      this.open = false
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!--    这里是新增的内容-->
+    <f-dialog id="dialogger" ref="child" :style-object="styleObject">
+      <button @click="closeThis">关闭</button>
+    </f-dialog>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import FDialog from '@/components/Dialog/index.vue'
+
+export default {
+  name: 'Dashboard',
+  components: { FDialog },
+  data() {
+    return {
+      styleObject: {
+        width: '50%',
+        height: '100%'
+      }
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  methods: {
+    closeThis() {
+      this.$refs.child.closeDialog()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
